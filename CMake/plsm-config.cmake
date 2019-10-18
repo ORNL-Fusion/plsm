5c0ad1e0a0858ae3bea476579304d6d3242f4d83
Philip Fackler <facklerpw@ornl.gov>
2019-10-18 16:18:31 -0400

    Added Kokkos dependency to installed config file

--- CMake/plsm-config.cmake
+++ CMake/plsm-config.cmake
@@ -2,3 +2,7 @@
 
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" DIRECTORY)
 include("${SELF_DIR}/plsm.cmake")
+
+include(CMakeFindDependencyMacro)
+list(APPEND CMAKE_PREFIX_PATH ${Kokkos_DIR})
+find_dependency(Kokkos)
